976dd83730fe5d2d93dcdd18aa8c5ec038fc091a
rodvama <rod_valen_ma@hotmail.com>
2020-11-22 17:53:16 -0600

    Ultimo commit

--- front/MoneyLife/src/environments/environment.ts
+++ front/MoneyLife/src/environments/environment.ts
@@ -5,7 +5,7 @@
 export const environment = {
   production: false,
   // apiEndPoint:'',
-  apiEndPoint: 'https://c3a6a203bf9f.ngrok.io/'
+  apiEndPoint: 'https://112708a3b3ba.ngrok.io/'
   // apiEndPoint: 'https://moneylifev1.azurewebsites.net/'
   // apiEndPoint: 'http://localhost:8000/'
 };
